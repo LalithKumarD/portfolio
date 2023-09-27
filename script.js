2810ab98c15556063e07104c9ce56cafdd09cbe3
LalithKumarD <136110482+LalithKumarD@users.noreply.github.com>
2023-09-27 06:49:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Script running")
+ document.querySelector('.cross').style.display = 'none';
+ document.querySelector('.hambuger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display='inline'
+        document.querySelector('.cross').style.display='none'
+    }
+    else{
+        document.querySelector('.cross').style.display='inline'
+        document.querySelector('.ham').style.display='none' 
+    }
+
+ });
+ 
